6ca14861e9e20986c73b11247dc2a47c573a8db8
Martin Kukla <martin.kukla@cantab.net>
2025-04-08 10:06:37 +0000

    cleaning

--- distributed-ffn.py
+++ distributed-ffn.py
@@ -1,5 +1,4 @@
 ### PARAMS + MODEL
-INIT_SCALE = 2e-2 # In my previous AIYAIN experiment, I used 0.1. TODO XXX:  setup up Xavier/Glorot for AIYAIN instead?
 
 import math
 import torch
@@ -8,11 +7,11 @@ import torch
 
 DEVICE = "cuda"
 
-def init_linear_layer(m, n, scale=INIT_SCALE): 
+def init_linear_layer(m, n, scale=2e-2): 
     return scale * torch.randn((n, m), device=DEVICE)
     
-def init_tlayer_ffn(emb_dim, ffn_dim, residual_scale=INIT_SCALE):
-    return [init_linear_layer(emb_dim, ffn_dim)] +  [init_linear_layer(ffn_dim, emb_dim, residual_scale)]
+def init_tlayer_ffn(emb_dim, ffn_dim):
+    return [init_linear_layer(emb_dim, ffn_dim)] +  [init_linear_layer(ffn_dim, emb_dim)]
 
 
 ### MODEL
@@ -40,13 +39,15 @@ def tlayer_ffn_bkwd(dloss_dx, layer_params, x):
 
     return dloss_dx.reshape(x_in.shape), (ffn1_dloss_dp, ffn2_dloss_dp)
 
-#### BASELINE 1GPU
+#### Setup:
 
 BS, D = 32, 16
 FFN = 4 * D
 x = torch.randn((BS, D), device="cuda")
 dloss_dx = torch.randn((BS, D), device="cuda")
 layer_params = init_tlayer_ffn(D, FFN)
+
+#### BASELINE 1GPU
 
 # Forward
 y = tlayer_ffn_fwd(layer_params, x)
@@ -57,3 +58,8 @@ dloss_dx, dloss_dp = tlayer_ffn_bkwd(dloss_dx, layer_params, x)
 print('dloss_dx', dloss_dx.shape, dloss_dx[:10, :10])
 print('dloss_dp[0]', dloss_dp[0].shape, dloss_dp[0][:10, :10])
 print('dloss_dp[1]', dloss_dp[1].shape, dloss_dp[1][:10, :10])
+
+# Optimizer step (just SGD for now)
+n_layer_params =tuple([p-0.001*g for p, g in zip(layer_params, dloss_dp)])
+print(f'n_layer_params[0]', n_layer_params[0].shape, n_layer_params[0][:10, :10])
+print(f'n_layer_params[1]', n_layer_params[1].shape, n_layer_params[1][:10, :10])
