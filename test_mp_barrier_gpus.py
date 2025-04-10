ac622195b81c735a6a4d1880ca29be0c2ac8a803
Martin Kukla <martin.kukla@cantab.net>
2025-04-10 11:25:00 +0000

    more testing

--- test_mp_barrier_gpus.py
+++ test_mp_barrier_gpus.py
@@ -11,7 +11,9 @@ def init_pool_processes(the_barrier, the_gpus_outputs):
     gpus_outputs = the_gpus_outputs
 
 def add_rank(local_rank):
-    gpus_outputs[local_rank].add_(local_rank)
+    #gpus_outputs[local_rank].add_(local_rank)
+    gpus_outputs[local_rank][0].add_(local_rank)
+    gpus_outputs[local_rank][1].add_(local_rank)
     barrier.wait()
     if local_rank==0:
         print("Inside", gpus_outputs)
@@ -20,8 +22,13 @@ if __name__ == '__main__':
     
     mp.set_start_method('spawn')
 
-    output = torch.zeros((2,2))
-    gpus_output= [torch.clone(output).cuda(i) for i in range(nGPUs)] 
+    #output = torch.zeros((2,2))
+    #gpus_output= [torch.clone(output).cuda(i) for i in range(nGPUs)] 
+    output = (torch.zeros((2,2)), torch.zeros((2,2)))
+    def clone_output(output, i):
+        return (torch.clone(output[0]).cuda(i), torch.clone(output[1]).cuda(i))
+    gpus_output= [clone_output(output, i) for i in range(nGPUs)] 
+    
     barrier = mp.Barrier(nGPUs)
     with mp.Pool(nGPUs, initializer=init_pool_processes, initargs=(barrier, gpus_output)) as p:
         p.map(add_rank, range(nGPUs))
