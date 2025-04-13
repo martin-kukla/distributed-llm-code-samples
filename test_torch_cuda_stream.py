5938f84e98cfbc2e56a12df82e96c1c6530c5332
Martin Kukla <martin.kukla@cantab.net>
2025-04-13 13:08:31 +0000

    bug in pytorch, can't create communicator

--- test_torch_cuda_stream.py
+++ test_torch_cuda_stream.py
@@ -8,6 +8,17 @@ a_tensors = [torch.randn((M, N), device=i) for i in range(nGPUs)]
 b_tensors = [torch.randn((N, K), device=i) for i in range(nGPUs)]
 outputs = [None] * nGPUs
 
+# do I need torch.cuda.synchronize() here?
+
+# PyTorch's bug: https://github.com/pytorch/pytorch/issues/38019
+# uid = nccl.unique_id()
+# print(uid)
+# comms = []
+# for i in range(nGPUs):
+#     with torch.cuda.device(i):
+#         c = nccl.init_rank(nGPUs, uid, i)
+#         comms.append(c)
+
 for i, s_and_ts in enumerate(zip(streams, a_tensors, b_tensors)):
     s, a, b = s_and_ts
     with torch.cuda.stream(s):
@@ -15,5 +26,5 @@ for i, s_and_ts in enumerate(zip(streams, a_tensors, b_tensors)):
             outputs[i] = torch.matmul(a, b)
             # TODO make the below works
             #nccl.all_reduce(outputs, streams=streams)
-torch.cuda.synchronize()
+torch.cuda.synchronize() # do I need this?
 print(outputs)
