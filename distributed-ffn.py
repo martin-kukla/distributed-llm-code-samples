f3b37ab9601661c89f36da63bf3ffbd637267824
Martin Kukla <martin.kukla@cantab.net>
2025-04-15 08:38:36 +0000

    WIP: FSDP

--- distributed-ffn.py
+++ distributed-ffn.py
@@ -1,8 +1,17 @@
+# This is a toy example how to distribute Transformer's FFN sublocks among many GPUs
+# It shows how to implmement DDP and FSDP almost from the first principle:
+# Initially, I meant to use torch.cuda.nccl directly, but there is a known issue (https://github.com/pytorch/pytorch/issues/38019).
+# Thus, using torch.distributed's init_process_group + its communication collectives.
+#
+# To test, run python distributed-ffn.py -i 16 -bs 8192 -d 8192
+#
+# Remaining TODO: overlap communication with computation 
+
 import argparse
 import math
 import os
 import torch
-import torch.distributed as dist # we will use init_process_group and communication collectives (all_reduce, all_gather and reduce_scatter)
+import torch.distributed as dist # we will only use init_process_group and communication collectives (all_reduce etc.)
 #import torch.cuda.nccl as nccl
 import multiprocessing as mp
 import time
@@ -127,7 +136,63 @@ def train_ddp(layer_params, seeds, batch_size):
     return gpus_layer_params[0]
 
 # FSDP
+def train_process_fsdp(local_rank, layer_params, seeds, batch_size):
+    # Probably we don't need to specify it explicitly, as the default group will do. TODO: confirm
+    #group = dist.new_group(range(nGPUs))
+    gen=torch.Generator()
 
+    sharded_p0 = layer_params[0]
+    sharded_p0s = [torch.zeros_like(sharded_p0).cuda(local_rank) for _ in range(nGPUs)]
+    dist.all_gather(sharded_p0s, sharded_p0) 
+    sharded_p1 = layer_params[1]
+    sharded_p1s = [torch.zeros_like(sharded_p1).cuda(local_rank) for _ in range(nGPUs)]
+    dist.all_gather(sharded_p1s, sharded_p1)
+    
+    layer_params = (torch.cat(sharded_p0s), torch.cat(sharded_p1s, dim=1))
+    #if local_rank==0:
+    #    print(f'local_rank {local_rank}', layer_params[0].shape, layer_params[1].shape)
+    #    print(f'local_rank {local_rank}', layer_params[0], layer_params[1])
+    model_size = layer_params[0].shape[1]
+
+    for seed in seeds.numpy().tolist():
+        # get data
+        gen.manual_seed(seed)
+        x = torch.randn((batch_size, model_size), generator=gen).cuda(local_rank)
+        dloss_dx = torch.randn((batch_size, model_size), generator=gen).cuda(local_rank)
+
+        # Forward
+        y = tlayer_ffn_fwd(layer_params, x)
+
+        # Backward
+        dloss_dp = tlayer_ffn_bkwd(dloss_dx, layer_params, x)[1]
+        dist.all_reduce(dloss_dp[0], op=dist.ReduceOp.SUM) #, group=group)       
+        dist.all_reduce(dloss_dp[1], op=dist.ReduceOp.SUM) #, group=group)
+        
+        # Optimzer (in place)
+        for param, grad in zip(layer_params, (dloss_dp[0], dloss_dp[1])):
+            param.add_(-LR*grad)
+  
+def train_fsdp(layer_params, seeds, batch_size):
+    assert len(seeds) % nGPUs == 0
+
+    def chunk_p(p, dim):
+       return [p_chunk.cuda(i) for i, p_chunk in enumerate(p.chunk(nGPUs, dim=dim))]
+    chunked_params = [chunk_p(p, dim=i%2) for i, p in enumerate(layer_params)]
+    gpus_layer_params = list(map(list, zip(*chunked_params)))
+    cpus_seeds = [t.reshape(-1) for t in seeds.reshape((-1, nGPUs)).chunk(nGPUs, dim=1)]
+
+    processes = []
+    mp.set_start_method('spawn')
+    for rank in range(nGPUs):
+        p = mp.Process(target=init_process, args=(rank, gpus_layer_params[rank], cpus_seeds[rank], batch_size, train_process_fsdp))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
+
+    return gpus_layer_params[0]
+    
 #### Setup:
 
 if __name__ == '__main__':
@@ -149,15 +214,16 @@ if __name__ == '__main__':
     print(f'PARAMS: {num_params:_} (size {_gb(num_params)} GB)')
     print(f'\n')
 
+    print(f'initial layer_params', layer_params[0].shape, layer_params[1].shape)
     print(f'initial layer_params', layer_params[0][:5,:5], layer_params[1][:5,:5])
     
-    fns = [train_1gpu, train_ddp] # , train_fsdp]
+    fns = [train_1gpu, train_ddp, train_fsdp]
     for i, fn in enumerate(fns):
         if args.mode==0 or args.mode==i+1:
             t0 = time.time()
             fn_layer_params = fn(layer_params, seeds, args.batch_size)
             t1 = time.time()
-            print(f'{fn.__name__} takes {t1-t0} seconds: ', fn_layer_params[0][:5,:5], fn_layer_params[1][:5,:5])
+            print(f'{fn.__name__} takes {t1-t0} seconds: ', fn_layer_params[0].shape, fn_layer_params[0][:5,:5], fn_layer_params[1].shape, fn_layer_params[1][:5,:5])
 
     # if args.mode==0:
     #     assert torch.allclose(n_layer_params_ddp[0], n_layer_params_1gpu[0]), f"n_layer_params_ddp[0] {n_layer_params_ddp[0]} n_layer_params_1gpu[0] {n_layer_params_1gpu[0]}"
